9b25ef43f4a04cabe71fbc9d3a3a561296f0f1df
Josh <josh@Jasons-MacBook-Air-2.local>
2015-01-05 02:14:55 -0500

    Adding tests for #startGame, #levelUp, #renderMole, and #showGameBoard

--- test/unit/game/module.spec.js
+++ test/unit/game/module.spec.js
@@ -1,8 +1,8 @@
 describe('game/module', function() {
 	beforeEach(function() {
-		this.collection = { collection: true, add: stub() };
+		this.collection = { collection: true, add: stub(), findWhere: stub() };
 		this.view = { view: true };
-		this.model = { model: true }
+		this.model = { model: true, set: stub(), active: false }
 
 		this.Collection = stub().returns(this.collection);
 		this.Model = stub().returns(this.model);
@@ -77,16 +77,70 @@ describe('game/module', function() {
 		});
 	});
 
-	// describe('#startGame', function() {
-	// 	beforeEach(function() {
-	// 		stub(this.timer, new Date())
-	// 	});
+	describe('#startGame', function() {
+		beforeEach(function() {
+			this.timer = sinon.useFakeTimers();
+			this.renderMole = sinon.spy();
+			setInterval(this.renderMole, 1100);
+		});
 
-	// 	it('should start a timer and call #renderMole', function() {
-			
-	// 	})
-	// })
+		it('should start a timer and call #renderMole when the interval is reached', function() {
+			expect(this.renderMole).to.not.be.called;
+			this.timer.tick(1100);
+			expect(this.renderMole).to.be.called;
+			this.timer.tick(1100);
+			expect(this.renderMole).to.be.calledTwice;
+		});
 
+		afterEach(function() {
+			this.timer.restore();
+		});
+	});
+
+	describe('#levelUp', function() {
+		beforeEach(function() {
+			this.clock = sinon.useFakeTimers();
+
+			this.speed = 1100;
+			this.renderMole = sinon.spy();
+			this.timer = setInterval(this.renderMole, this.speed);
+		});
+
+		it('should call #renderMole after leveling', function() {
+			this.clock.tick(this.speed);
+			expect(this.renderMole).to.be.called;
+		});
+
+		it('should have a lowered speed after leveling', function() {
+			this.speed -= 100;
+			this.timer = setInterval(this.renderMole, this.speed);
+			this.clock.tick(1000);
+
+			expect(this.renderMole).to.be.called;
+		});
+	});
+
+	describe('#renderMole', function() {
+		beforeEach(function() {
+			this.collection.findWhere.returns(this.model);
+			this.module.renderMole();
+		});
+
+		it('should set the new model to active', function() {
+			expect(this.model.set).to.be.calledWith('active', true);
+		});
+	});
+
+	describe('#showGameBoard', function() {
+		beforeEach(function() {
+			this.module._showGameBoard();
+		});
+
+		it('should show the container with the collection', function() {
+			expect(this.collection.add).to.be.called;
+			expect(this.container.show).to.be.calledWith(this.view);
+		});
+	});
 
 
 });
